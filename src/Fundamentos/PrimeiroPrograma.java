9f4d5353a41a6fbe9ce869da3564cb1e32ea8fb6
Weslei23 <sschmittweslei@gmail.com>
2025-05-18 14:20:29 -0300

    atualização

--- src/Fundamentos/PrimeiroPrograma.java
+++ src/Fundamentos/PrimeiroPrograma.java
@@ -1,7 +1,9 @@
 package Fundamentos;
 
-public class PrimeiroPrograma {
-    public static void main(String[] args) {
+public class PrimeiroPrograma
+{
+    public static void main( String[] args )
+    {
         System.out.println("Hello world!");
     }
 }
---
+++ src/Fundamentos/TipoString.java
@@ -0,0 +1,16 @@
+package Fundamentos;
+
+public class TipoString
+{
+    public static void main( String[] args )
+    {
+        String texto = "Aqui temos um texto em String";
+
+        String nome = "Weslei";
+        String sobreNome = "Schmitt";
+
+        System.out.println( texto );
+
+        System.out.println( nome + " " + sobreNome );
+    }
+}
